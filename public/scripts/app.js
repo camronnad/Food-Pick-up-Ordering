a8a4ce363d24c0e876a2103c556c58d7b7d8a113
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-07-28 15:15:21 -0700

    Fix : render menu and render order in the carticon

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,32 @@
-// Client facing scripts here
 $( document ).ready(function() {
-  $('.fa-shopping-cart').click(function(){
-    alert('clicked on me')
+  $('logout-btn').click(function(){
+    window.location.href = '/';
   })
 });
+
+$( document ).ready(function() {
+  $('btn-btn-primary').click(function(){
+
+        $(".orderButton").toggleClass("active");
+      //loadOrder function removed here
+
+        $.ajax({
+          url: "/api/users/order",
+          method: "POST",
+          dataType: "json",
+          success: function(response) {
+            if (response.success) {
+              showConfirmationPopup();
+              // Send SMS to the restaurant
+              sendSMS();
+            }
+          },
+          error: function(error) {
+            // Handle any error that occurs during the AJAX request
+            console.error("Error placing order:", error);
+            // Show an error message to the user
+            showErrorMessage("Error placing order. Please try again later.");
+          }
+        });
+      });
+    })
